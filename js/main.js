05baf1ce7da8d770403c1ba1e40e1f0e77930cdf
Mônica Mendes <monicamendes@192-168-1-3.tpgi.com.au>
2019-03-13 20:23:33 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay = [];
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+};
